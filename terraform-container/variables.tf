a9e0b6eae07cbfe250a78ddf756a97299b746fc9
Jonathan-Nilsson <erik.jonathan.nilsson@gmail.com>
2022-10-18 15:32:11 +0200

    add web app & container registry

---
+++ terraform-container/variables.tf
@@ -0,0 +1,29 @@
+variable "environment" {
+  description = "The environment short name"
+  type        = string
+  default     = "lab"
+}
+
+variable "location_long" {
+  description = "The location where to place resources"
+  type        = string
+  default     = "north europe"
+}
+
+variable "location" {
+  description = "The location short name"
+  type        = string
+  default     = "ne"
+}
+
+variable "name" {
+  description = "The name to use for the different parts of the deployment"
+  type        = string
+  default     = "joni46151"
+}
+
+variable "container_name" {
+  description = "The name of the container that will be deployed to the Azure Web App"
+  type        = string
+  default     = "azure-lab"
+}
